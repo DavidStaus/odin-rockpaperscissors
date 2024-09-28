1961ea3f69dbc76c2fec18a684e65dfd81099d83
DavidSenn <178643637+DavidStaus@users.noreply.github.com>
2024-09-28 15:35:57 +0200

    finished human and player choice functions

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log("Hello, world!");
+
+
+// randomly choose 'Rock', 'Paper' or 'Scissors'.
+
+// randomly roll an integer between 1 and 3
+// output and return rock if integer = 1
+// output and return paper if integer = 2
+// output and return scissors if integer 3
+
+function getComputerChoice() {
+    switch(Math.ceil(Math.random() * 3)) {
+        case 1:
+            console.log('Computer chose: Rock');
+            return 'Rock';
+            break;
+        case 2:
+            console.log('Computer chose: Paper');
+            return 'Paper';
+            break;
+        case 3:
+            console.log('Computer chose: Scissors');
+            return 'Scissors';
+            break;
+    }
+}
+
+// prompt user for Rock Paper or Scissors
+// print answer to console
+
+function getHumanChoice() {
+    let humanChoice = prompt('Rock, Paper, or Scissors?');
+    if (humanChoice === 'Rock' || humanChoice ===  'Paper' || humanChoice === 'Scissors') {
+        console.log(humanChoice);
+        return humanChoice;
+        } else {
+        console.log('Not a valid answer!');
+        alert('Not a valid answer!');
+        getHumanChoice();
+        }
+}
+
+// init 2 variables called humanScore and computerScore
+// init them to 0
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+// take humanChoice and computerChoice and versus them
+// rock beat scissors
+// scissors beat paper
+// paper beats rock
+// output to console name of winner and why
+// winning choice score is incredmentet by 1
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+function playRound(humanChoice, computerChoice) {
+
+}
+
+// Running code //
+playRound()
